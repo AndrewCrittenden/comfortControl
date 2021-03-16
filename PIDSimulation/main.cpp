506a6d55fbe1e14493085f4388d93a6907b14d01
rocksinmypocket <79170630+rocksinmypocket@users.noreply.github.com>
2021-03-15 22:18:40 -0400

    Update main.cpp

--- PIDSimulation/main.cpp
+++ PIDSimulation/main.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * Created on February 20, 2021, 12:19 PM
  */
