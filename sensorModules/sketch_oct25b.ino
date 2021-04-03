31f9a8e4bc9d4b8187b6ce95a218e985a3487bc6
Bellrem <79170639+Bellrem@users.noreply.github.com>
2021-04-03 14:42:27 -0400

    Update sketch_oct25b.ino

--- sensorModules/sketch_oct25b.ino
+++ sensorModules/sketch_oct25b.ino
@@ -5,7 +5,7 @@ double Room_temp = 298.15;
 double Resistor_temp = 100000;
 double beta = 3950;
 double rThermistor = 0;
-double Balance_res = 100000;
+double Balance_res = 143000;
 int adcSamples[10];
 double adcAvg = 0;
 #include <math.h> //for log()
