72023d947b01a9d33380da41d3c76cdfb15db44d
Rose <50803400+herborspice@users.noreply.github.com>
2021-04-27 20:30:35 -0400

    Add files via upload
    Example of the arduino_secrets.h file for the sensor modules. Must be saved in the same folder as the arduino script.

---
+++ sensorModules/arduino_secrets.h
@@ -0,0 +1,13 @@
+//#define SECRET_SSID "DunderMifflin"  //HERE
+//#define SECRET_PASS "absolutelyido"
+
+
+#define SECRET_SSID "COMFORT"  //HERE
+#define SECRET_PASS "ashrae11"
+
+//#define SECRET_SSID "SM-G973Ub54"  //HERE
+//#define SECRET_PASS "0000005763"
+
+
+//#define SECRET_SSID "David Oliver's iPhone"  //HERE
+//#define SECRET_PASS "q44aprumtrxva"
